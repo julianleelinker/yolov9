89e6c18293152e3fb77e8b2aee862558af536fea
julianleelinker <julianlee@linkervision.com>
2024-03-27 18:02:45 +0800

    Done data split

--- parser_innov_bdd_anno.py
+++ parser_innov_bdd_anno.py
@@ -3,7 +3,10 @@ import orjson
 import pathlib
 import tqdm
 import pandas as pd
+import numpy as np
+import random
 from collections import defaultdict
+import copy
 import argparse
 
 
@@ -84,7 +87,7 @@ uninterested_classes = {
 
 final_classes = [
     'ANIMAL',
-    'BARRICADE',
+    'BARRICADE', 
     'BICYCLE',
     'BUS',
     'BUS_STOP_SIGN',
@@ -99,16 +102,16 @@ final_classes = [
     'PEDESTRIAN',
     'POLE',
     'RIDER',
-    'ROAD_CRACKS',
-    'ROAD_PATCH',
-    'ROAD_POTHOLES',
+    'ROAD_CRACKS', #
+    'ROAD_PATCH', #
+    'ROAD_POTHOLES', #
     'STOP_SIGN',
     'TRAFFIC_LIGHT',
     'TRUCK',
     'UGLY_TRAFFIC_SIGN',
-    'UNCLEAR_LANE_MARKING',
-    'UNCLEAR_ROAD_MARKING',
-    'UNCLEAR_STOP_LINE',
+    'UNCLEAR_LANE_MARKING', #
+    'UNCLEAR_ROAD_MARKING', #
+    'UNCLEAR_STOP_LINE', #
     'WHEELCHAIR',
 ]
 
@@ -153,10 +156,32 @@ def create_soft_link(image_root, lidar_plane_url):
         print(f'create soft link: link {link_path} -> source {actual_path}')
 
 
+def create_random_mask(num_images):
+    pattern = [False]*10
+    pattern[2:8] = [True]*6
+    rand_ints = random.sample(range(num_images//len(pattern)), num_images//len(pattern)//5)
+    val_mask = np.full(num_images, False, dtype=bool)
+    train_mask = np.full(num_images, True, dtype=bool)
+    for idx in rand_ints:
+        val_mask[idx*len(pattern):(idx+1)*len(pattern)] = pattern
+        train_mask[idx*len(pattern):(idx+1)*len(pattern)] = [False]*10
+    return val_mask, train_mask
+
+
+def find_good_split(focus_stats, target_ratio=0.12/0.8, tolerance=0.03):
+    condition = False
+    while not condition:
+        val_mask, train_mask = create_random_mask(focus_stats.shape[0])
+        ratio = np.divide(focus_stats[val_mask].sum(axis=0), focus_stats[train_mask].sum(axis=0))
+        condition = np.logical_and(ratio < target_ratio+tolerance, ratio > target_ratio-tolerance).all()
+        print(ratio, condition)
+    return val_mask, train_mask
+
+
 def main(args):
     file_path_root = '/home/julian/data/indus-innov/raw_anno'
     image_root     = '/home/julian/data/indus-innov/images/kaohsiung5gsmartcitydemo'
-    bdd_anno_root  = '/home/julian/data/indus-innov/0325/bdd_anno'
+    bdd_anno_root  = '/home/julian/data/indus-innov/split-0327/bdd_anno'
     file_path_list = sorted(pathlib.Path(file_path_root).glob('*.json'))
     # file_path_list = file_path_list[7:8]
 
@@ -167,27 +192,30 @@ def main(args):
     task_dict = {}
     df = pd.DataFrame(columns=(['timestamp', 'image_id', 'seq_id'] + final_classes))
 
+    all_annos = {}
     for file_path in file_path_list:
         # create folder
         print(f'parsing {file_path}')
         with open(file_path, 'r') as f:
             data = orjson.loads(f.read())
         root_path = f'{bdd_anno_root}/{pathlib.Path(file_path).stem}'
-        pathlib.Path(root_path).mkdir(exist_ok=True, parents=True)
 
 
-        for key, val in tqdm.tqdm(data['jobBddData'].items()):
+        for key, value in tqdm.tqdm(data['jobBddData'].items()):
             assert key not in task_dict, f'{key} already exist'
-            task_dict[key] = val
-            pathlib.Path(f'{root_path}/{key}').mkdir(exist_ok=True, parents=True)
+            task_dict[key] = value
+            # pathlib.Path(f'{root_path}/{key}').mkdir(exist_ok=True, parents=True)
 
             bdddata_id = 0 # 0 or 1?
-            bdddata = val[bdddata_id]['bddData']
-            for frame_id, frame in enumerate(bdddata['frame_list']):
+            bdddata = value[bdddata_id]['bddData']
+
+            all_annos[key] = {}
+            for idx, frame in enumerate(bdddata['frame_list']):
                 # frame info for splitting
                 timestamp = get_timestamp_from_file_name(frame['name'])
                 image_id = (df['timestamp'] == timestamp).sum()
                 class_counter = {cat: 0 for cat in final_classes}
+                all_annos[key][(timestamp, image_id)] = frame
 
                 # create soft link
                 create_soft_link(image_root, frame['lidarPlaneURLs'])
@@ -244,13 +272,13 @@ def main(args):
                 df = pd.concat([df, pddata], ignore_index=True)
 
             # save processed bdd to json file
-            if not args.dry:
-                json_path = f'{root_path}/{key}/{bdddata_id}.json'
-                with open(json_path, 'w') as f:
-                    f.write(orjson.dumps(bdddata).decode())
+            # if not args.dry:
+            #     json_path = f'{root_path}/{key}/{bdddata_id}.json'
+            #     with open(json_path, 'w') as f:
+            #         f.write(orjson.dumps(bdddata).decode())
 
 
-    df = df.sort_values(by='timestamp', ascending=True)
+    df = df.sort_values(by=['image_id', 'timestamp'], ascending=True)
     sequence_time_diff = 5*10**8 # 0.5 sec
     cur_seq_id = 0
     last_timestamp = df.iloc[0]['timestamp']
@@ -261,12 +289,40 @@ def main(args):
         df.at[idx, 'seq_id'] = cur_seq_id
         last_timestamp = row['timestamp']
     print(df.sum())
-    import ipdb; ipdb.set_trace()
-
-
+    stats_array = df[final_classes].to_numpy(dtype=int)
+    focus_stats = stats_array[:, [16,17,18,23,24,25]]
+    target_ratio, tolerance = 0.12/0.8, 0.05
+    val_mask, train_mask = find_good_split(focus_stats, target_ratio=target_ratio, tolerance=tolerance)
     print(f'newly added labels: {newly_added_categories}')
     print(f'not_supported_labels: {len(not_supported_labels)}')
+    print(f'done spliting with target_ratio {target_ratio} and tolerance {tolerance}')
+
+    # save split id
+    for key, frame_list in tqdm.tqdm(all_annos.items()):
+        bdddata_train = copy.deepcopy(bdddata)
+        bdddata_val = copy.deepcopy(bdddata)
+        bdddata_train['frame_list'] = []
+        bdddata_val['frame_list'] = []
+        pathlib.Path(f'{bdd_anno_root}/train/{key}').mkdir(exist_ok=True, parents=True)
+        pathlib.Path(f'{bdd_anno_root}/val/{key}').mkdir(exist_ok=True, parents=True)
+        for (timestamp, image_id) in frame_list:
+            # check train or val
+            index = df.index[(df['timestamp'] == timestamp) & (df['image_id'] == image_id)].tolist()
+            index = df.index.get_loc(index[0])
+            if train_mask[index]:
+                bdddata_train['frame_list'].append(frame_list[(timestamp, image_id)])
+            elif val_mask[index]:
+                bdddata_val['frame_list'].append(frame_list[(timestamp, image_id)])
+
+        json_path = f'{bdd_anno_root}/train/{key}/{bdddata_id}.json'
+        with open(json_path, 'w') as f:
+            f.write(orjson.dumps(bdddata_train).decode())
+
+        json_path = f'{bdd_anno_root}/val/{key}/{bdddata_id}.json'
+        with open(json_path, 'w') as f:
+            f.write(orjson.dumps(bdddata_val).decode())
     print(f'bdd anno parse to {bdd_anno_root} done!')
+    import ipdb; ipdb.set_trace()
 
 
 if __name__ == '__main__':
