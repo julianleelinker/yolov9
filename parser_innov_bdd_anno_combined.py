0299b38c42587e3992d1d06f8cc287c392e66754
julianleelinker <julianlee@linkervision.com>
2024-05-14 16:00:54 +0800

    Use a map to store class grouping information

--- parser_innov_bdd_anno_combined.py
+++ parser_innov_bdd_anno_combined.py
@@ -88,32 +88,36 @@ uninterested_classes = {
 }
 
 
-final_classes = [
-    'ANIMAL',
-    'BARRICADE',
-    'BICYCLE',
-    'BUS',
-    'BUS_STOP_SIGN',
-    'CAR',
-    'CLEAN_TRAFFIC_SIGN', # 'CLEAN_STOP_SIGN',
-    'CONE',
-    'DIRTY_TRAFFIC_SIGN', # 'DIRTY_STOP_SIGN', #
-    'ETC',
-    'HUMAN_LIKE',
-    'JERSEY_BARRIER',
-    'MOTORCYCLE',
-    'NON_UPRIGHT',
-    'PEDESTRIAN',
-    'RIDER',
-    'ROAD_CRACKS',
-    'ROAD_PATCH',
-    'ROAD_POTHOLES', #
-    'TRAFFIC_LIGHT',
-    'TRUCK',
-    'UNCLEAR_ROAD_MARKING', #'UNCLEAR_LANE_MARKING', 'UNCLEAR_STOP_LINE'
-    'WHEELCHAIR',
-]
-final_classes = sorted(final_classes)
+final_classes_map = {
+    'ANIMAL':               'ANIMAL',
+    'BARRICADE':            'BARRICADE',
+    'BICYCLE':              'BICYCLE',
+    'BUS':                  'BUS',
+    'BUS_STOP_SIGN':        'BUS_STOP_SIGN',
+    'CAR':                  'CAR',
+    'CLEAN_STOP_SIGN':      'CLEAN_TRAFFIC_SIGN',
+    'CLEAN_TRAFFIC_SIGN':   'CLEAN_TRAFFIC_SIGN',
+    'CONE':                 'CONE',
+    'DIRTY_STOP_SIGN':      'DIRTY_TRAFFIC_SIGN',
+    'DIRTY_TRAFFIC_SIGN':   'DIRTY_TRAFFIC_SIGN',
+    'ETC':                  'ETC',
+    'HUMAN_LIKE':           'HUMAN_LIKE',
+    'JERSEY_BARRIER':       'JERSEY_BARRIER',
+    'MOTORCYCLE':           'MOTORCYCLE',
+    'NON_UPRIGHT':          'NON_UPRIGHT',
+    'PEDESTRIAN':           'PEDESTRIAN',
+    'RIDER':                'RIDER',
+    'ROAD_CRACKS':          'ROAD_CRACKS',
+    'ROAD_PATCH':           'ROAD_PATCH',
+    'ROAD_POTHOLES':        'ROAD_POTHOLES',
+    'TRAFFIC_LIGHT':        'TRAFFIC_LIGHT',
+    'TRUCK':                'TRUCK',
+    'UNCLEAR_LANE_MARKING': 'UNCLEAR_ROAD_MARKING',
+    'UNCLEAR_ROAD_MARKING': 'UNCLEAR_ROAD_MARKING',
+    'UNCLEAR_STOP_LINE':    'UNCLEAR_ROAD_MARKING',
+    'WHEELCHAIR':           'WHEELCHAIR',
+}
+final_classes = sorted(list(set(list(final_classes_map.values()))))
 interested_classes_ids = [8, 18]
 
 
@@ -297,13 +301,8 @@ def main(args):
                             else:
                                 assert False, f'wired attribute value: {label["attributes"]["DIRTY"]}'
 
-                        # combine label as MZ ask
-                        if label['category'] in ['UNCLEAR_LANE_MARKING', 'UNCLEAR_STOP_LINE', 'UNCLEAR_ROAD_MARKING']:
-                            label['category'] = 'UNCLEAR_ROAD_MARKING'
-                        elif label['category'] in ['CLEAN_STOP_SIGN', 'CLEAN_TRAFFIC_SIGN']:
-                            label['category'] = 'CLEAN_TRAFFIC_SIGN'
-                        elif label['category'] in ['DIRTY_STOP_SIGN', 'DIRTY_TRAFFIC_SIGN']:
-                            label['category'] = 'DIRTY_TRAFFIC_SIGN'
+                        # combine label finally
+                        label['category'] = final_classes_map[label['category']]
                         
 
                         class_counter[label['category']] += 1
